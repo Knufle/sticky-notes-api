0f62793506d2920f4c706c340235641ab26f9e78
Knufle <iamknufle@gmail.com>
2022-03-17 16:41:14 -0300

    feat: updating port variable

--- src/server.ts
+++ src/server.ts
@@ -8,10 +8,11 @@ import errorHandler from './errors/handler';
 import cors from 'cors';
 
 const app = express();
+const port = process.env.PORT || 3333;
 
 app.use(cors());
 app.use(express.json());
 app.use(errorHandler);
 app.use(routes);
 
-app.listen(process.env.PORT || 3333);
+app.listen(port);
